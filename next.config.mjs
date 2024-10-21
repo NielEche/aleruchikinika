57669be861191621b4232135d5e6c6d4ceb45030
Niel Eche <nieleche@Niel-Eche-MacBook-Pro.local>
2024-10-21 05:29:20 +0100

    updated build

--- next.config.mjs
+++ next.config.mjs
@@ -1,16 +1,16 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'images.ctfassets.net',
-          port: '', // Leave this empty if you're using the default port
-          pathname: '/**', // Allows all images from this domain
-        },
-      ],
-    },
-  };
-  
-  export default nextConfig;
-  
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'images.ctfassets.net',
+        port: '', // Leave this empty if you're using the default port
+        pathname: '/**', // Allows all images from this domain
+      },
+    ],
+  },
+  output: 'export', // Enable static export
+};
+
+export default nextConfig;
